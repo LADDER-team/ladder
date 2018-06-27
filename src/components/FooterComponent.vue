03b29ef7a36698cc4a9272d492d958407a1b4fa6
tetty0217 <tettaro_01291978@ezweb.ne.jp>
2018-06-27 23:35:28 +0900

    added toolbar and footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-footer
+            app
+            fixed
+            class="g-footer">
+      <span class="g-footer-text">&copy; 2018</span>
+    </v-footer>
+</template>
+<style scoped lang="sass">
+    .g-footer
+        background: #BBDEFB
+    .g-footer-text
+        margin: 0 auto
+</style>
