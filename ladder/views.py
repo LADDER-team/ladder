29b336b99306300dc304d97b75ef8f791ba6c814
funashi <pinkgreen0304@gmail.com>
2018-06-27 11:17:38 +0900

    登録時重複回避機能

--- ladder/views.py
+++ ladder/views.py
@@ -4,6 +4,8 @@ from .serializers import TagsSerializer,LadderSerializer,UserSerializer,UnitSeri
 from django_filters import rest_framework as filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated,AllowAny,IsAdminUser
 from rest_framework.authentication import BasicAuthentication,TokenAuthentication
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
@@ -26,7 +28,33 @@ class LadderViewSet(viewsets.ModelViewSet,permissions.BasePermission):
     queryset = Ladder.objects.all().filter(is_public=True)
     serializer_class = LadderSerializer
     filter_class = LadderFilter
-    permission_classes = (IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def create(self, request, *args, **kwargs):
+        if {'title':request.data['title']} in Ladder.objects.filter(creater=request.user.pk).values('title'):
+            raise ValidationError('同じタイトルのLADDERが投稿されています')
+        add_data = request.data.copy()
+        add_data['creater'] = request.user.pk
+        serializer = self.get_serializer(data=add_data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        add_data = request.data.copy()
+        add_data['creater'] = request.user.pk
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=add_data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -56,10 +84,60 @@ class TagViewSet(viewsets.ModelViewSet):
 class LinkViewSet(viewsets.ModelViewSet):
     queryset = Link.objects.all()
     serializer_class = LinkSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def create(self, request, *args, **kwargs):
+        if {'latter':int(request.data['latter'])} in Link.objects.filter(user=request.user.pk).values('latter'):
+            raise ValidationError('既にこのLADDERはペグされています')
+        add_data = request.data.copy()
+        add_data['user'] = request.user.pk
+        serializer = self.get_serializer(data=add_data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        add_data = request.data.copy()
+        add_data['user'] = request.user.pk
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=add_data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
 
 
 class LearningStatusViewSet(viewsets.ModelViewSet):
     queryset = LearningStatus.objects.all()
     serializer_class = LearningStatusSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)
+
+    def create(self, request, *args, **kwargs):
+        if {'unit':int(request.data['unit'])} in LearningStatus.objects.filter(user=request.user.pk).values('unit'):
+            raise ValidationError('同じタイトルのLADDERが投稿されています')
+        add_data = request.data.copy()
+        add_data['user'] = request.user.pk
+        serializer = self.get_serializer(data=add_data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        add_data = request.data.copy()
+        add_data['user'] = request.user.pk
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=add_data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
