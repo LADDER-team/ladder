9871cdd1a8f6d019968828626ef5baf192138a74
tetty0217 <tettaro_01291978@ezweb.ne.jp>
2018-06-28 08:27:03 +0900

    added ladder component

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,23 @@
         <ToolBar/>
         <v-content>
             <v-container fluid fill-height>
-                <v-layout justify-center align-center wrap class="getApiWrap">
-                    <h2 class="headline headTitle">Ladder title</h2>
-
+                <v-layout justify-center align-center wrap>
+                    <v-flex md3 justify-center align-cener>
+                        <div class="ladder-item ladder-item-active">
+                            <p>unit:{{ ladderList.unit[0].index }}</p>
+                            <p>{{ ladderList.unit[0].title }}</p>
+                        </div>
+                        <div class="ladder-item">
+                            <p>unit:{{ ladderList.unit[1].index }}</p>
+                            <p>{{ ladderList.unit[1].title }}</p>
+                        </div>
+                        <div class="ladder-item">
+                            <p>unit:{{ ladderList.unit[2].index }}</p>
+                            <p>{{ ladderList.unit[2].title }}</p>
+                        </div>
+                    </v-flex>
+                    <v-flex md7 justify-center align-start></v-flex>
+                    <v-flex md2 justify-center align-cener></v-flex>
                 </v-layout>
             </v-container>
         </v-content>
@@ -33,14 +47,12 @@
       right: true,
       rightDrawer: false,
       title: 'Vuetify.js',
-      users: []
+      ladderList: []
     }),
-    mounted() {
-      console.log('mounted!')
-
-      axios.get('/api/users/')
+    mounted(){
+      axios.get('/api/ladder/6/')
         .then((response) => {
-          this.users = response.data
+          this.ladderList = response.data
         })
         .catch((error) => {
           console.log(error)
@@ -48,7 +60,7 @@
     },
     components: {
       'ToolBar': ToolBarComponent,
-      'Footer': FoorerComponent
+      'Footer': FoorerComponent,
     }
   }
 </script>
@@ -56,4 +68,13 @@
     .headTitle
         width: 100%
         text-align: center
+    .ladder-item
+        background: #CFD8DC
+        padding: 10px
+        p
+            margin: 0
+            color: #fff
+            font-weight: bold
+    .ladder-item-active
+        background: #64B5F6
 </style>
---
+++ src/components/LadderComponent.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: "ladder-component",
+    data(){
+      return{
+        ladders: [],
+        id: 6
+      }
+    },
+    mounted(){
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
